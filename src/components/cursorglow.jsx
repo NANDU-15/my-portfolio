9439ee4925d7495269c4f0a5aeba4f18ffbdfcf4
Nandu <nandunaidujaddu@gmail.com>
2026-05-26 18:36:37 +0530

    fix

--- src/components/cursorglow.jsx
+++ src/components/cursorglow.jsx
@@ -1,30 +1,5 @@
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-
-import Hero from "./sections/Hero";
-import About from "./sections/About";
-import Skills from "./sections/Skills";
-import Projects from "./sections/Projects";
-import Achievements from "./sections/Achievements";
-import Experience from "./sections/Experience";
-import Contact from "./sections/Contact";
-
-function App() {
-  return (
-    <main className="bg-black text-white overflow-x-hidden relative">
-      <Navbar />
-
-      <Hero />
-      <About />
-      <Skills />
-      <Projects />
-      <Achievements />
-      <Experience />
-      <Contact />
-
-      <Footer />
-    </main>
-  );
+function CursorGlow() {
+  return null;
 }
 
-export default App;gi
+export default CursorGlow;
