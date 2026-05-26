e3c0f1612c29b704d3db32a78d0f497d39980979
Nandu <nandunaidujaddu@gmail.com>
2026-05-26 18:15:49 +0530

    fresh clean deploy

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-import Lenis from "@studio-freight/lenis";
+import Lenis from "lenis";
 
 const lenis = new Lenis({
   duration: 1.2,
