b2e0da465f6b7b0b8c21a1633eccf2b16fd8d4c9
Nandu <nandunaidujaddu@gmail.com>
2026-05-26 20:49:34 +0530

    bug fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./App.jsx";
 import "./index.css";
-
 import Lenis from "lenis";
+import { useEffect } from "react";
 
-const lenis = new Lenis({
-  duration: 1.2,
-  smoothWheel: true,
-});
+function Root() {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      smoothWheel: true,
+    });
 
-function raf(time) {
-  lenis.raf(time);
-  requestAnimationFrame(raf);
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  }, []);
+
+  return <App />;
 }
-
-requestAnimationFrame(raf);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>
 );
